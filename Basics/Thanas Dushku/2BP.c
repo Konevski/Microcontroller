8b1d200d238bd6cf309d2184b03d6f25a577ec1b
Thanas-Dushku <thanas.dushku@stud.hshl.de>
2021-10-18 13:30:17 +0200

    Update 2BP.c

--- Basics/Thanas Dushku/2BP.c
+++ Basics/Thanas Dushku/2BP.c
@@ -2,37 +2,37 @@
 #include <stdlib.h>
 
 int state = 1;  //the initial state of the predictor
-int bit = 1;    //the input bit
+int event = 1;  //the input event
 
-int main(){
+void control(int event){
 	switch (state) {   //the main code of the predictor
 	case 1:            //strong not-taken
-		if (bit == 1) {       
+		if (event == 1) {       
 			state = 2;  //switching to weak not-taken
 		}
 		break;
 
 	case 2:            //weak not-taken
-		if (bit == 0) {
+		if (event == 0) {
 			state = 1;   //switching to strong not-taken
 		}
-		else if (bit == 1) {
+		else if (event == 1) {
 			state = 3;    //switching to weak taken
 		}
 		break;
 
 	case 3:           //strong taken
-		if (bit == 0) {
+		if (event == 0) {
 			state = 2;  //switching to weak not-taken
 		}
-		else if (bit == 1) {
+		else if (event == 1) {
 			state = 3;   //switching to strong taken
 		}
 		break;
 
 
 	case 4:           //strong taken 
-		if (bit == 0) {
+		if (event == 0) {
 			state = 2;   //switching to weak taken
 		}
 		
