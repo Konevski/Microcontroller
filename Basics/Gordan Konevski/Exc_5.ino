a2689e1725178594bcd39557e8271bf068ec142e
Konevski <56386762+Konevski@users.noreply.github.com>
2021-11-17 10:36:30 +0100

    Update Exc_5.ino

--- Basics/Gordan Konevski/Exc_5.ino
+++ Basics/Gordan Konevski/Exc_5.ino
@@ -2,13 +2,13 @@
 //SPI COMMUNICATION BETWEEN TWO ARDUINO 
 //CIRCUIT DIGEST
 
-#include<SPI.h>                             //Library for SPI 
+#include <SPI.h>                             //Library for SPI 
 #define LED 7           
 #define ipbutton 2
 int buttonvalue;
 int x;
-void setup (void)
 
+void setup (void)
 {
   Serial.begin(115200);                   //Starts Serial Communication at Baud Rate 115200 
   
@@ -20,23 +20,17 @@ void setup (void)
   digitalWrite(SS,HIGH);                  // Setting SlaveSelect as HIGH (So master doesnt connnect with slave)
 }
 
-void loop(void)
+void loop (void)
 {
   byte Mastersend,Mastereceive;          
-
   buttonvalue = digitalRead(ipbutton);   //Reads the status of the pin 2
 
   if(buttonvalue == HIGH)                //Logic for Setting x value (To be sent to slave) depending upon input from pin 2
-  {
     x = 1;
-  }
   else
-  {
     x = 0;
-  }
   
   digitalWrite(SS, LOW);                  //Starts communication with Slave connected to master
-  
   Mastersend = x;                            
   Mastereceive=SPI.transfer(Mastersend); //Send the mastersend value to slave also receives value from slave
   
@@ -45,29 +39,34 @@ void loop(void)
     digitalWrite(LED,HIGH);              //Sets pin 7 HIGH
     Serial.println("Master LED ON");
   }
+  
   else
   {
    digitalWrite(LED,LOW);               //Sets pin 7 LOW
    Serial.println("Master LED OFF");
   }
+  
   delay(1000);
+
 }
 
-Slave Arduino Code:
+/////////////////////////////////////////
+
+//Slave Arduino Code:
 //SPI SLAVE (ARDUINO)
 //SPI COMMUNICATION BETWEEN TWO ARDUINO 
 //CIRCUIT DIGEST
 //Pramoth.T
 
-#include<SPI.h>
+#include <SPI.h>
 #define LEDpin 7
 #define buttonpin 2
 volatile boolean received;
 volatile byte Slavereceived,Slavesend;
 int buttonvalue;
 int x;
-void setup()
 
+void setup()
 {
   Serial.begin(115200);
   
@@ -90,13 +89,14 @@ ISR (SPI_STC_vect)                        //Inerrrput routine function
 
 void loop()
 { 
-  if(received)                            //Logic to SET LED ON OR OFF depending upon the value recerived from master
+  if (received)                            //Logic to SET LED ON OR OFF depending upon the value recerived from master
    {
       if (Slavereceived==1) 
       {
         digitalWrite(LEDpin,HIGH);         //Sets pin 7 as HIGH LED ON
         Serial.println("Slave LED ON");
-      }else
+      }
+      else
       {
         digitalWrite(LEDpin,LOW);          //Sets pin 7 as LOW LED OFF
         Serial.println("Slave LED OFF");
@@ -108,13 +108,12 @@ void loop()
       {
         x=1;
         Serial.print("Thank you");
-      }else
-      {
-        x=0;
       }
-      
-  Slavesend=x;                             
-  SPDR = Slavesend;                           //Sends the x value to master via SPDR 
-  delay(1000);
-}
+      else
+        x=0;
+
+    Slavesend=x;                             
+    SPDR = Slavesend;                           //Sends the x value to master via SPDR 
+    delay(1000);
+  }
 }
